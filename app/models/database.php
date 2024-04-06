149d9736c9199c87ad4df053337354c8ae305264
haikalnuril <haikal.nuril23@gmail.com>
2024-04-06 20:13:51 +0700

    feat: model

---
+++ app/models/database.php
@@ -0,0 +1,24 @@
+<?php
+
+Class Dashboard{
+    public $id;
+    public $user_id;
+    public $owner;
+    public $no_hp;
+    public $created_at;
+    public $updated_at;
+    public $judul;
+    
+    static function select(){
+  
+    }
+    static function insert(){
+  
+    }
+    static function update(){
+  
+    }
+    static function delete(){
+  
+    }
+  }
